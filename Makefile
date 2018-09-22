86c13568227f3464a33e29ef3d5ed4011ef85f42
Rory Bradford <roryrjb@gmail.com>
2018-09-22 15:46:08 +0100

    shell linting

---
+++ Makefile
@@ -0,0 +1,4 @@
+test:
+	shellcheck *.bash
+
+.PHONY: test
--- docker-setup.bash
+++ docker-setup.bash
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
+set -euo pipefail
 
 versions=(1.6.3 1.8.1 1.10.3 1.12.2 1.14.0)
 
 for version in "${versions[@]}"
 do
     curl \
-        -L https://nginx.org/download/nginx-$version.tar.gz \
-        -o /tmp/nginx_$version.tar.gz
+        -L "https://nginx.org/download/nginx-$version.tar.gz" \
+        -o "/tmp/nginx_$version.tar.gz"
 
     cd /tmp
-    mkdir nginx_$version
-    cat nginx_$version.tar.gz | gunzip | tar -x
+    mkdir "nginx_$version"
+    tar -xz < "nginx_$version.tar.gz"
 
-    cd nginx-$version
+    cd "nginx-$version"
     ls
 
     ./configure \
-        --prefix=/root/nginx_$version \
+        --prefix="/root/nginx_$version" \
         --add-module=/tmp/ngx-system-info
 
     make
